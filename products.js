6c230f9e819b0cd409a2d19af577cf8328661919
Rocio <rocio.povolo46@gmail.com>
2026-03-03 21:45:57 -0300

    Updates

---
+++ products.js
@@ -0,0 +1,20 @@
+// assert { type: "json" } ya no va
+// https://stackoverflow.com/questions/70106880/err-import-assertion-type-missing-for-import-of-json-file
+import productsList from "./products.json" with { type: "json" };
+function getProductsSortedByPrice() {
+    return [...productsList].sort((a, b) => a.price - b.price);
+}
+export function productsListComponent() {
+    const section = document.createElement("section");
+    section.style.border = "solid 1px #ccc";
+    section.style.padding = "10px";
+    const list = document.createElement("ul");
+    const sortedProducts = getProductsSortedByPrice();
+    sortedProducts.forEach((product) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${product.title} - $${product.price}`;
+        list.appendChild(listItem);
+    });
+    section.appendChild(list);
+    return section;
+}
